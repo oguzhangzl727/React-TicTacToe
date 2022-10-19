cd1dd841b69a342c2c4f353054645e5d27bf0c86
Oğuzhan Güzel <oguzhangzl727@gmail.com>
2022-10-20 02:35:48 +0300

    React-TicTacToe
    Reactjs Ve Socket.io Kullanarak TicTacToe Oyunu

--- src/server.js
+++ src/server.js
@@ -2,18 +2,11 @@ var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http, { cors: { origin: "*" } });
 var port = 3001;
-app.get("/", function (req, res) {
-  res.send("Youtube Videosu");
-});
+
 
 io.on("connection", function (socket) {
   console.log("connection oldu");
-  socket.on("startGame", (data) => {
-    socket.join("oda1");
-    socket.to("oda1").emit("newPlayer", "yeni oyuncu  odaya katıldı.");
-    socket.emit("startGame", { message: "odaya katılındı." });
-  });
-
+  
   socket.on("send_data", (data) => {
     socket.broadcast.emit("send_data", {
       position: data.position,
